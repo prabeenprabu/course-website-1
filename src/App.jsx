303bfc2d81db5110432d4a692d6a8cd83b57d71e
prabeen <prabeenprabu@gmail.com>
2024-04-23 18:58:48 +0530

    programs section component finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Component/Navbar/Navbar";
 import Hero from "./Component/Hero/Hero";
 import Programs from "./Component/Programs/Programs";
+import Title from "./Component/Title/Title";
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Title subtitle="our programs" title="What we offer" />
             <Programs />
         </div>
     );
