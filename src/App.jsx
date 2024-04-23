0b64949290dd3d6e9785e4d3f0dbd0cc40421734
prabeen <prabeenprabu@gmail.com>
2024-04-23 18:22:04 +0530

    program section component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Component/Navbar/Navbar";
 import Hero from "./Component/Hero/Hero";
+import Programs from "./Component/Programs/Programs";
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Programs />
         </div>
     );
 };
---
+++ src/Component/Programs/Programs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Programs.css";
+import program_1 from "../../assets/program-1.png";
+import program_2 from "../../assets/program-2.png";
+import program_3 from "../../assets/program-3.png";
+
+const Programs = () => {
+    return (
+        <div className="programs container">
+            <div className="program">
+                <img src={program_1} alt="" />
+                <img src={program_2} alt="" />
+                <img src={program_3} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Programs;
