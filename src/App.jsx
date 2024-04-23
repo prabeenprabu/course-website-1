40890a6c89c8b457b5f054f186ea24de75a9574e
prabeen <prabeenprabu@gmail.com>
2024-04-23 17:29:48 +0530

    navbar structure & styling work finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Navbar from "./Component/Navbar/Navbar";
 
 const App = () => {
-    return <div>App</div>;
+    return <div>
+      <Navbar/>
+    </div>;
 };
 
 export default App;
