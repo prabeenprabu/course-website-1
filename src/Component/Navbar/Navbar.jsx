a394fc0d10109d3360b1fcf5c7f9fd3dd19ad853
prabeen <prabeenprabu@gmail.com>
2024-04-24 18:38:47 +0530

    react-scroll have added successfully

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
     const [sticky, setSticky] = useState(false);
@@ -15,13 +16,51 @@ const Navbar = () => {
         <nav className={` container ${sticky ? " dark-nav" : ""}`}>
             <img src={logo} alt="logo" className="logo" />
             <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About Us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
                 <li>
-                    <button className="btn">Contact Us</button>
+                    <Link to="hero" smooth={true} offset={0} duration={700}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="program"
+                        smooth={true}
+                        offset={-250}
+                        duration={700}>
+                        Program
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" smooth={true} offset={-100} duration={700}>
+                        About Us
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="campus"
+                        smooth={true}
+                        offset={-210}
+                        duration={700}>
+                        Campus
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="testimonials"
+                        smooth={true}
+                        offset={-250}
+                        duration={700}>
+                        Testimonials
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="contact"
+                        smooth={true}
+                        offset={-250}
+                        duration={700}>
+                        <button className="btn">Contact Us</button>
+                    </Link>
                 </li>
             </ul>
         </nav>
